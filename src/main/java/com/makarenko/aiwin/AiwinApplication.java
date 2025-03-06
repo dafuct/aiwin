e8c5cd89e1f6caa43c93aea39bec9222abafbbba
dafuct <dafuct@gmail.com>
2025-03-06 14:12:28 +0200

    spring boot template

---
+++ src/main/java/com/makarenko/aiwin/AiwinApplication.java
@@ -0,0 +1,13 @@
+package com.makarenko.aiwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiwinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makarenko/aiwin/AiwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makarenko.aiwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
